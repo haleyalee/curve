278d203eda098945d1675f4027b605317a16b757
Haley <haleyalee@gmail.com>
2020-08-27 14:59:45 -0400

    dock

--- app/components/Dock.js
+++ app/components/Dock.js
@@ -3,7 +3,7 @@
 import React, { Component } from 'react';
 import { View, Image, Text, Button, TouchableOpacity } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
-import { AntDesign, Feather, MaterialCommunityIcons, MaterialIcons, FontAwesome } from '@expo/vector-icons';
+import { AntDesign, Feather, MaterialCommunityIcons, MaterialIcons, FontAwesome, Octicons } from '@expo/vector-icons';
 
 import styles from '../Styles';
 
@@ -16,9 +16,9 @@ export class Dock extends Component {
           {/* Home */}
           <AntDesign name="home" size={30} color="#235789" onPress={() => this.props.navigation.navigate('HomeScreen')}/>
           {/* History */}
-          <MaterialCommunityIcons name="history" size={24} color="#235789" onPress={() => this.props.navigation.navigate('HistoryScreen')} />
+          <MaterialCommunityIcons name="history" size={30} color="#235789" onPress={() => this.props.navigation.navigate('HistoryScreen')} />
           {/* Profile */}
-          <MaterialIcons name="person-outline" size={24} color="#235789" onPress={() => this.props.navigation.navigate('ProfileScreen')}/>
+          <Octicons name="person" size={30} color="#235789" onPress={() => this.props.navigation.navigate('ProfileScreen')}/>
         </View>
       </View>
     )
