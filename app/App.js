174f1c7e9306f9b5dd986ca87ce962b9350faa4e
Haley <haleyalee@gmail.com>
2020-08-28 13:29:30 -0400

    added link to CMP and redir to Home

--- app/App.js
+++ app/App.js
@@ -37,7 +37,7 @@ function App() {
   return (
     <NavigationContainer>
          <Stack.Navigator 
-          initialRouteName="Home"
+          initialRouteName="HomeScreen"
           screenOptions={{ headerShown: false}}>
           <Stack.Screen
             name="LoginScreen"
